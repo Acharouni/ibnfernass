2dc76ad505090e73f75fb6b10ad04d7eeea1a3d3
Acharouni <aymencharouni@outlook.fr>
2024-04-05 16:35:10 +0100

    2 commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    document.getElementById("mobile-menu").style.display = "block";
+    document.querySelector(".togg").style.display = "none";
+    document.querySelector(".hid").style.display = "block";
+}
+function NoTshowMenu() {
+    document.getElementById("mobile-menu").style.display = "none";
+    document.querySelector(".togg").style.display = "block";
+    document.querySelector(".hid").style.display = "none";
+}
